c9fc480776f0056ee4655c20cf676e95a0712be6
bhavya121999 <bhavyagupta1203@gmail.com>
2020-06-15 12:54:05 +0530

    Created the controller for Application

--- src/main/java/com/idr/forms/resource/impl/FormResourceImpl.java
+++ src/main/java/com/idr/forms/resource/impl/FormResourceImpl.java
@@ -1,21 +1,22 @@
 package com.idr.forms.resource.impl;
 
+import com.idr.forms.FieldsRepository;
 import com.idr.forms.domain.Fields;
-import com.idr.forms.resource.Resource;
-import com.idr.forms.service.FormService;
+//import com.idr.forms.resource.Resource;
+//import com.idr.forms.service.FormService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
-@RestController
+/*@RestController
 @RequestMapping("/forms")
 @CrossOrigin(origins="http://localhost:3000")
-public class FormResourceImpl implements Resource<Fields> {
+public class FormResourceImpl  {
 
     @Autowired
     private FormService formService;
@@ -44,4 +45,36 @@ public class FormResourceImpl implements Resource<Fields> {
     public ResponseEntity<Fields> deleteById(Long id) {
         return new ResponseEntity<>(formService.deleteById(id), HttpStatus.OK);
     }
+}
+
+ */
+
+@RestController
+public class FormResourceImpl{
+
+    @Autowired
+    FieldsRepository fieldsRepository;
+
+    @CrossOrigin(origins="http://localhost:3000")
+    @GetMapping(path="/forms")
+    public List<Fields> getAllUsers(){
+        List<Fields> fields = new ArrayList<>();
+        fieldsRepository.findAll().forEach(fields :: add);
+        return fields;
+    }
+
+    @CrossOrigin(origins="http://localhost:3000")
+    @PostMapping(path="/forms")
+    public Fields addUser(@RequestBody Fields fields){
+        fieldsRepository.save(fields);
+        return fields;
+    }
+
+    @CrossOrigin(origins="http://localhost:3000")
+    @DeleteMapping(path="/forms/{id}")
+    public void deleteUser(@PathVariable long id){
+        fieldsRepository.deleteById(id);
+    }
+
+
 }

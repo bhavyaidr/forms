fe057ad5499d40a36b980388f2491f142d907ddf
bhavya121999 <bhavyagupta1203@gmail.com>
2020-06-15 12:52:34 +0530

    1. Updated the application.properties file 2. Created Repository Interface

---
+++ src/main/java/com/idr/forms/FieldsRepository.java
@@ -0,0 +1,9 @@
+package com.idr.forms;
+
+import com.idr.forms.domain.Fields;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface FieldsRepository extends JpaRepository<Fields,Long>
+{
+}
